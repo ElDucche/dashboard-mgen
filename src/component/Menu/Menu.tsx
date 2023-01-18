c3472a3b5de4685d0658f8897107f6a72453d800
ElDucche <82090419+ElDucche@users.noreply.github.com>
2023-01-18 14:46:07 +0100

    1 file modified

--- src/component/Menu/Menu.tsx
+++ src/component/Menu/Menu.tsx
@@ -2,12 +2,12 @@ import MenuItem from '../Menu/MenuItem';
 
 function Menu() {
   return (
-    <div className="h-screen bg-mgen/10 w-52 opacity-12 flex justify-center">
+    <div className="h-screen bg-mgen/10 w-52 opacity-12 flex justify-center fixed">
       <ul className="mt-8 p-2 w-full h-64 flex flex-col justify-around">
-        <MenuItem title="Accueil" icon={'fa-solid fa-house text-mgen w-4'} />
+        <MenuItem title="Accueil" icon={'fa-solid fa-house text-mgen'} />
         <MenuItem
           title="Évènements"
-          icon={'fa-solid fa-calendar-days text-mgen w-8'}
+          icon={' fa-solid fa-calendar-days text-mgen'}
         />
         <MenuItem title="Utilisateur" icon={'fa-solid fa-users text-mgen'} />
         <MenuItem
